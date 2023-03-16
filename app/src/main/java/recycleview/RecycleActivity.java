48a86b609589b070f95943c55425739248176be6
jasontatum123 <641672016@qq.com>
2023-03-16 16:34:39 +0800

    test

---
+++ app/src/main/java/recycleview/RecycleActivity.java
@@ -0,0 +1,16 @@
+package recycleview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.myapplication.R;
+
+public class RecycleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recycle);
+    }
+}
